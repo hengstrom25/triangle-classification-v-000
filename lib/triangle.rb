638f4733b4261501e87a9fd6fe953e67405571e3
Heidi Engstrom <hengstrom25@gmail.com>
2018-05-22 19:41:46 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,7 +5,7 @@ class Triangle
     @side_two = side_two
     @side_three - side_three
     
-    if @side_one > (side_two + side_three)
+    if @side_one >= (side_two + side_three) || 
       raise TriangleError
     end
   end
