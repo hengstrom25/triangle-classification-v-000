e4d55249562f7f8a30c6024f140f8b0fb1dff340
Heidi Engstrom <hengstrom25@gmail.com>
2018-05-22 19:43:24 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,11 @@
 class Triangle
   
-  def initialize(side_one, side_two, side_three)
-    @side_one = side_one
-    @side_two = side_two
-    @side_three - side_three
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 - s3
     
-    if side_one >= (side_two + side_three) || side_two >= (side_one + side_three) || side_three >= (side_two + side_one)
+    if s1 >= (s2 + s3) || side_two >= (side_one + side_three) || side_three >= (side_two + side_one)
       raise TriangleError
     end
   end
