7e9163c2591ef5e501135d9316e90490b8317055
Heidi Engstrom <hengstrom25@gmail.com>
2018-05-22 19:37:01 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,11 @@
 class Triangle
   
   def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three - side_three
+    
+    
   end
   
   def kind
